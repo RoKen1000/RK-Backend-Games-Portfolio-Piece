a078dc148eb1c09f21c34373761ca8a04c2a03f4
RoKen1000 <rosskennedy993@outlook.com>
2023-03-28 18:37:43 +0100

    Create test and functionality for the /api/reviews endpoint to check if it correctly returns the required array of objects with the addition of a comment_count property.

--- db/app.js
+++ db/app.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const {selectAllCategories} = require("./controllers/categories.controller")
 const {notFound, badRequest, malformedEndpoint, internalServerError} = require("./error-handling/errors")
-const {selectReview} = require("./controllers/reviews.controller")
+const {selectReview, selectAllReviews} = require("./controllers/reviews.controller")
 
 
 const app = express();
 
 app.get("/api/categories", selectAllCategories);
 app.get("/api/reviews/:review_id", selectReview);
+app.get("/api/reviews", selectAllReviews);
 app.all("/api/*", malformedEndpoint)
 
 // app.use(malformedEndpoint);
